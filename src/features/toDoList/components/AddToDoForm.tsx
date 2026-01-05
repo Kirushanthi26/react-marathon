f4417ae6961cf0a3ad7a2890dfe0300a85f227c3
Kirushanthi26 <kirushanthikiru26@gmail.com>
2026-01-05 18:40:49 +0530

    AddToDoForm

--- src/features/toDoList/components/AddToDoForm.tsx
+++ src/features/toDoList/components/AddToDoForm.tsx
@@ -1,12 +1,45 @@
+import { useState, type FormEvent } from "react"
+import type { ToDo } from "../types/ToDoType";
 
 
 const AddToDoForm = () => {
+    const [toDoName, setToDoName] = useState<string>("");
+    const [description, setDescription] = useState<string>("");
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        const newToDo: ToDo = {
+            id: Math.random().toString(),
+            toDoName,
+            description,
+            isDone: false
+        }
+
+        console.log(newToDo)
+
+        setToDoName("")
+        setDescription("")
+    }
+
+
     return (
-        <div className="bg-gray-800 p-5 space-y-3 w-1/2 m-auto">
-            <h1 className="text-3xl font-bold text-amber-200 text-center">To Do List</h1>
-            <input type="text" placeholder="To Do" className="border border-amber-100 bg-amber-50 w-full p-2" />
-            <input type="text" placeholder="To Do Description" className="border border-amber-100 bg-amber-50 w-full p-2" />
-            <button type="button" className="bg-amber-200 w-full p-3">Add to list</button>
+        <div className="bg-gray-800 p-5 w-1/2 m-auto">
+            <h1 className="text-3xl font-bold text-amber-200 text-center mb-2">To Do List</h1>
+
+            <form onSubmit={handleSubmit} className="space-y-3">
+                <input type="text" placeholder="To Do"
+                    className="border border-amber-100 bg-amber-50 w-full p-2"
+                    onChange={(e) => setToDoName(e.target.value)}
+                    value={toDoName} />
+
+                <input type="text" placeholder="To Do Description"
+                    className="border border-amber-100 bg-amber-50 w-full p-2"
+                    onChange={(e) => setDescription(e.target.value)}
+                    value={description} />
+
+                <button type="submit" className="bg-amber-200 w-full p-3">Add to list</button>
+            </form>
         </div>
     )
 }
