9732d2cf3537168597dae7325634cbbe985592f8
Kirushanthi26 <kirushanthikiru26@gmail.com>
2026-01-07 16:55:16 +0530

    jest fix

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,5 +2,13 @@
 module.exports = {
   preset: 'ts-jest',
   setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.jest.json',
+      },
+    ],
+  },
 };
--- src/features/toDoList/components/AddToDoForm.test.tsx
+++ src/features/toDoList/components/AddToDoForm.test.tsx
@@ -1,5 +1,5 @@
-import { render, screen } from '@testing-library/react'
-import AddToDoForm from './AddToDoForm'
+import { render, screen } from "@testing-library/react";
+import AddToDoForm from "./AddToDoForm";
 
 
 test('it shows two inputs and a button', () => {
@@ -14,7 +14,7 @@ test('it shows two inputs and a button', () => {
     const button = screen.getByRole('button', { name: /add to list/i })
 
     //Assertion - make sure the component is doing what we expect it to do 
-    expect(inputs).toHaveLength(2)
-    expect(button).toBeInTheDocument()
+    expect(inputs).toHaveLength(2);
+    expect(button).toBeInTheDocument();
 
 })
