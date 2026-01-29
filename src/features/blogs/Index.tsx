af4c6f1e4450973ae2ea24143811a64850a0466a
Kirushanthi26 <kirushanthikiru26@gmail.com>
2026-01-29 12:26:13 +0530

    adding query client and query client provider

--- src/features/blogs/Index.tsx
+++ src/features/blogs/Index.tsx
@@ -1,12 +1,16 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import Post from "./Post"
 
+const queryClient = new QueryClient()
 
 const BlogAppIndex = () => {
     return (
-        <div className="bg-amber-50 h-screen">
-            <h1>Post Blog - Learn React Query</h1>
-            <Post />
-        </div>
+        <QueryClientProvider client={queryClient}>
+            <div className="bg-amber-50 h-screen">
+                <h1>Post Blog - Learn React Query</h1>
+                <Post />
+            </div>
+        </QueryClientProvider>
     )
 }
 
