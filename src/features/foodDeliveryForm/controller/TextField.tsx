87a73546a62a52548eafaf3bd7bbd4ce86c0dc1a
Kirushanthi26 <kirushanthikiru26@gmail.com>
2026-01-30 23:30:24 +0530

    reusable component for input field

---
+++ src/features/foodDeliveryForm/controller/TextField.tsx
@@ -0,0 +1,24 @@
+import { forwardRef, type ForwardedRef } from "react"
+import type { FieldError } from "react-hook-form"
+
+type TextFieldProps = React.InputHTMLAttributes<HTMLInputElement> & {
+    label: string
+    error?: FieldError
+}
+
+export const TextField = forwardRef((props: TextFieldProps, ref: ForwardedRef<HTMLInputElement>) => {
+    const { type = "text", label, className = "", error, ...other } = props
+    return (
+        <div>
+            <label>{label}</label>
+            <br />
+            <input
+                type={type}
+                className={`border border-gray-500 p-2 my-2 ${className}`}
+                ref={ref}
+                {...other}
+            />
+            <p className="text-red-500">{error?.message}</p>
+        </div>
+    )
+})
--- src/features/foodDeliveryForm/foodDeliveryFormRHF/FoodDeliveryFormRHF.tsx
+++ src/features/foodDeliveryForm/foodDeliveryFormRHF/FoodDeliveryFormRHF.tsx
@@ -1,4 +1,6 @@
 import { useForm } from "react-hook-form"
+import { TextField } from "../controller/TextField"
+
 
 type UserDetails = {
     customerName: string
@@ -26,12 +28,14 @@ export default function FoodDeliveryFormRHF() {
 
     return (
         <form onSubmit={handleSubmit(handleSubmitForm)} noValidate className="space-y-5 w-full">
-            <div className="grid grid-cols-2  gap-5">
-                <div>
-                    <input type="text" placeholder="Order number" className="border border-gray-500 p-2" {...register('orderNo')} disabled />
-                </div>
-                <div>
-                    <input type="number" placeholder="Mobile Number" className="border border-gray-500 p-2" {...register('mobileNo', {
+            <div className="grid grid-cols-2 gap-5">
+
+                <TextField label="Order number" {...register('orderNo')} disabled />
+
+                <TextField
+                    type="number"
+                    label="Mobile Number"
+                    {...register('mobileNo', {
                         required: {
                             value: true,
                             message: "mobile is require"
@@ -44,30 +48,33 @@ export default function FoodDeliveryFormRHF() {
                             value: 10,
                             message: "mobile number must be 10"
                         }
-                    })} />
-                    {errors.mobileNo && <p className="text-red-500">{errors.mobileNo.message}</p>}
-                </div>
-                <div>
-                    <input type="text" placeholder="Customer Name" className="border border-gray-500 p-2" {...register('customerName', {
+                    })}
+                    error={errors.mobileNo}
+                />
+
+                <TextField
+                    {...register('customerName', {
                         required: "customer name is require"
-                    })} />
-                    {errors.customerName && <p className="text-red-500">{errors.customerName.message}</p>}
-                </div>
-                <div>
-                    <input type="email" placeholder="email" className="border border-gray-500 p-2" {...register('email', {
-                        required: "email is require",
-                        pattern: {
-                            value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-                            message: "please enter valid email"
-                        },
-                        validate: {
-                            notFake: (value) => {
-                                return value !== "email@gmail.com" || "this email Blocked"
-                            }
+                    })}
+                    label="Customer Name"
+                    error={errors.customerName}
+                />
+
+                <TextField {...register('email', {
+                    required: "email is require",
+                    pattern: {
+                        value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                        message: "please enter valid email"
+                    },
+                    validate: {
+                        notFake: (value) => {
+                            return value !== "email@gmail.com" || "this email Blocked"
                         }
-                    })} />
-                    {errors.email && <p className="text-red-500">{errors.email.message}</p>}
-                </div>
+                    }
+                })}
+                    type="email"
+                    label="email"
+                    error={errors.email} />
             </div>
             <button type="submit" className="bg-amber-200 p-2">submit</button>
         </form>
