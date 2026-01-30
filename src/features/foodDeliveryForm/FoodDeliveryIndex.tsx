442526fe6e84efe69f236a0e725512be22c200b1
Kirushanthi26 <kirushanthikiru26@gmail.com>
2026-01-30 20:05:03 +0530

    simple RHF

--- src/features/foodDeliveryForm/FoodDeliveryIndex.tsx
+++ src/features/foodDeliveryForm/FoodDeliveryIndex.tsx
@@ -1,11 +1,11 @@
-import FoodDeliveryForm from "./FoodDeliveryForm";
-//import FoodDeliveryFormRHF from "./foodDeliveryFormRHF/FoodDeliveryFormRHF";
+//import FoodDeliveryForm from "./FoodDeliveryForm";
+import FoodDeliveryFormRHF from "./foodDeliveryFormRHF/FoodDeliveryFormRHF";
 
 export default function FoodDeliveryIndex() {
     return (
         <div className="m-5 bg-pink-50 p-8">
-            <FoodDeliveryForm />
-            {/* <FoodDeliveryFormRHF/> */}
+            {/* <FoodDeliveryForm /> */}
+            <FoodDeliveryFormRHF />
         </div>
     )
 }
--- src/features/foodDeliveryForm/foodDeliveryFormRHF/FoodDeliveryFormRHF.tsx
+++ src/features/foodDeliveryForm/foodDeliveryFormRHF/FoodDeliveryFormRHF.tsx
@@ -1,9 +1,37 @@
+import { useForm } from "react-hook-form"
 
+type UserDetails = {
+    customerName: string
+    mobileNo: string
+}
 
 export default function FoodDeliveryFormRHF() {
-    return (
-        <div>
+    const { register, handleSubmit, formState: { errors } } = useForm<UserDetails>()
 
-        </div>
+    const handleSubmitForm = (data: UserDetails) => {
+        console.log(data)
+
+    }
+
+    const onError = (error: any) => {
+        console.log(error)
+    }
+
+    return (
+        <form onSubmit={handleSubmit(handleSubmitForm, onError)} className="space-y-5">
+            <div>
+                <input type="text" placeholder="Customer Name" className="border border-gray-500 p-2" {...register('customerName', {
+                    required: "customer name is require"
+                })} />
+                {errors.customerName && <p className="text-red-500">{errors.customerName.message}</p>}
+            </div>
+            <div>
+                <input type="number" placeholder="Mobile Number" className="border border-gray-500 p-2" {...register('mobileNo', {
+                    required: "mobile is require"
+                })} />
+                {errors.mobileNo && <p className="text-red-500">{errors.mobileNo.message}</p>}
+            </div>
+            <button type="submit" className="bg-amber-200 p-2">submit</button>
+        </form>
     )
 }
