e69d3e2cf0106c52448831ab9920d419b06d0e2e
Kirushanthi26 <kirushanthikiru26@gmail.com>
2026-02-19 15:42:28 +0530

    dynamic form - useFieldArray - CURD

--- src/features/foodDeliveryForm/foodDeliveryFormRHF/components/FoodItems.tsx
+++ src/features/foodDeliveryForm/foodDeliveryFormRHF/components/FoodItems.tsx
@@ -11,13 +11,31 @@ const FoodItems = () => {
     const { register } = useFormContext<UserDetails>()
     const { errors } = useFormState<UserDetails>({ name: "foodItems" })
 
-    const { fields } = useFieldArray<UserDetails>({ name: "foodItems" })
+    const { fields,
+        append, //Append input/inputs to the end of your fields and focus. 
+        remove,
+        update
+
+    } = useFieldArray<UserDetails>({ name: "foodItems" })
+
+    const handleAddRow = () => {
+        append({ name: "food append test", quantity: 8 }, {
+            focusName: "foodItems.0.quantity"
+        })
+    }
 
     return (
         <table>
             <tr>
                 <th>Food</th>
                 <th>Quantity</th>
+                <th>
+                    <button onClick={handleAddRow} className="bg-pink-500 p-1"
+                        type="button"
+                    >
+                        Add
+                    </button>
+                </th>
             </tr>
             {fields.map((field, index) =>
                 <tr key={field.id}>
@@ -41,8 +59,23 @@ const FoodItems = () => {
                             error={errors.foodItems && errors.foodItems[index]?.quantity}
                         />
                     </td>
+                    <td>
+                        <button type="button"
+                            onClick={() => remove(index)}
+                            className="bg-red-300 p-1"
+                        >
+                            delete
+                        </button>
+                    </td>
                 </tr>
             )}
+
+            <button type="button"
+                className="bg-yellow-200"
+                onClick={() => update(2, { name: "food update", quantity: 4 })}
+            >
+                update
+            </button>
         </table>
     )
 }
